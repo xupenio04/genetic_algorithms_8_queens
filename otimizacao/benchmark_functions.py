9ff00697cf9b9c739ea851d6c745dec549fc9162
LightAsh04 <lsa4@cin.ufpe.br>
2025-07-20 14:47:32 -0300

    adding limits to benchmark functions

--- otimizacao/benchmark_functions.py
+++ otimizacao/benchmark_functions.py
@@ -1,5 +1,21 @@
 import numpy as np
 
+ACKLEY_MAX_LIMIT = 32.768
+ACKLEY_MIN_LIMIT = -32.768
+ACKLEY_GLOBAL_MINIMUM_PARAM = 0
+
+RASTRIGIN_MAX_LIMIT = 5.12
+RASTRIGIN_MIN_LIMIT = -5.12
+RASTRIGIN_GLOBAL_MINIMUM_PARAM = 0
+
+SCHWEFEL_MAX_LIMIT = 500
+SCHWEFEL_MIN_LIMIT = -500
+SCHWEFEL_GLOBAL_MINIMUM_PARAM = 420.9687
+
+ROSENBROCK_MAX_LIMIT = 10
+ROSENBROCK_MIN_LIMIT = -5
+ROSENBROCK_GLOBAL_MINIMUM_PARAM = 1
+
 def ackley(vector:np.ndarray, dimensions:int=None, a:float=20, b:float=0.2, c:float=2*np.pi):
     if dimensions is not None and dimensions == vector.shape[0]:
         raise Exception("Number of dimensions is different from the vector shape")
@@ -31,4 +47,3 @@ def rosenbrock(vector:np.ndarray, dimensions:int=None, a:float=20, b:float=0.2, 
         somatorio += 100 * (vector[i+1] - vector[i]**2)**2 + (vector[i] - 1)**2
     return somatorio
             
-print(rastrigin(np.array([0,0,0,0,0,0,0])))
